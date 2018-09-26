86b1993f4808ba5ed144ab2791709fab74dbd0be
lq <844388470@qq.com>
2018-09-26 13:44:04 +0800

    Add:重构轨迹详情

--- pages/map/mapHistory/mapHistory.js
+++ pages/map/mapHistory/mapHistory.js
@@ -17,51 +17,24 @@ Page({
     circles: [],
     markerDatas: [],
     polyline: [],
-    optionType: 0,
+    list:[],
+    optionType: 3,
     distance:'',
     bushu:'',
-    kalu:''
+    kalu:'',
+    index:0,
+    isWifi:'',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      // app.request({
-      //   url: `${app.api.getHistory}${options.id}/positions`,
-      //   method: 'GET',
-      //   data: {
-      //     startTime: options.start,
-      //     endTime: options.end
-      //   }
-      // }).then(res => {
-        // let list = res || []
-        // let list = [
-        //   { longitude: 121.3406087, latitude: 31.1499699 },
-        //   { longitude: 121.3330688, latitude: 31.1612538 },
-        //   { longitude: 121.3293156, latitude: 31.166917 },
-        //   { longitude: 121.3268593, latitude: 31.1690175 },
-        //   { longitude: 121.3268943, latitude: 31.1689271 },
-        //   { longitude: 121.3265427, latitude: 31.1691976 },
-        //   { longitude: 121.3267516, latitude: 31.168909 },
-        //   { longitude: 121.3259663, latitude: 31.1695755 },
-        //   { longitude: 121.3264309, latitude: 31.1701394 },
-        //   { longitude: 121.3247445, latitude: 31.1717025 },
-        //   { longitude: 121.319554, latitude: 31.1755317 },
-        //   { longitude: 121.3189646, latitude: 31.1758857 },
-        //   { longitude: 121.3157018, latitude: 31.1810881 },
-        //   { longitude: 121.3142169, latitude: 31.1804959 },
-        //   { longitude: 121.3079582, latitude: 31.1885761 },
-        //   { longitude: 121.3070961, latitude: 31.1899618 },
-        //   { longitude: 121.3087381, latitude: 31.1927941 },
-        //   { longitude: 121.3036681, latitude: 31.1940063 },
-        // ]
-        // let list = [{ longitude: 121.363090, latitude: 31.124060 }, { longitude: 121.363268, latitude: 31.124365 }, { longitude: 121.363204, latitude: 31.124549 }, { longitude: 121.363590, latitude: 31.124691 }, { longitude: 121.364615, latitude: 31.122583 }, { longitude: 121.365591, latitude: 31.122840 }, { longitude: 121.366401, latitude: 31.123433 }, { longitude: 121.367002, latitude: 31.124209 }, { longitude: 121.367018, latitude: 31.124213 }, { longitude: 121.364996, latitude: 31.125844 }, { longitude: 121.364878, latitude: 31.125664 }, { longitude: 121.364545, latitude: 31.125343 }, { longitude: 121.363896, latitude: 31.124810 }, { longitude: 121.363596, latitude: 31.124686 }]
         let res = JSON.parse(options.data)
         let list = [...res]
         list = list.filter(res => res.longitude)
+
         if (!list.length) {
-          wx.hideLoading()
           wx.showToast({
             title: '无路径,请重新选择时间',
             icon: 'none',
@@ -70,50 +43,75 @@ Page({
           return
         }
         for(let i=0;i<list.length;i++){
-          if(i==0){
-            list[i]={
-             latitude: list[0].latitude, longitude: list[0].longitude, iconPath: '../../../image/map_index.png', width: 40, height: 50, anchor: { x: 0.5, y: 0.7 } 
-            }
-          }else if(i==list.length-1){
-            list[i]={
-              latitude: list[i].latitude, longitude: list[i].longitude, iconPath: '../../../image/map_index_h.png', width: 40, height: 50, anchor: { x: 0.5, y: 0.7 } 
-            }
-          }else{
-            list[i] = {
-              latitude: list[i].latitude, longitude: list[i].longitude, iconPath: '../../../image/yuan.png', width: 12, height: 12, anchor: { x: 0.5, y: 0.5}
-            }
+          list[i] = {
+            id: i, latitude: list[i].latitude, longitude: list[i].longitude, iconPath: '../../../image/yuan.png', width: 12, height: 12, anchor: { x: 0.5, y: 0.5 }, eventTime: list[i].eventTime, wifiGpsFlag: list[i].wifiGpsFlag
           }
         }
-        app.mapApi([list[0], list[list.length - 1]]).then(ress => {
-          this.setData({
-            optionType: 3,
-            time: `${res[0].eventTime.substr(0,10)}`,
-            longitude: list[0].longitude,
-            latitude: list[0].latitude,
-            timeFnNumber: `${res[0].eventTime.substr(11, 8)}-${res[res.length-1].eventTime.substr(11, 8)}`,
-            polyline: [{
-              points: list,
-              color: '#02ad00',
-              arrowLine:true,
-              width: 3,
-            }],
-            startName: ress[0],
-            endName: ress[1],
-            markerDatas: list,
-          })
+        this.setData({
+          list:list
         })
-        this.getdistance(list)
-        // this.getDistanceList(list)
-        // this.distances(list[0],list[1])
-        wx.hideLoading()
-      // }).catch(err => {
-      //   wx.hideLoading()
-      //   wx.showToast({
-      //     title: '获取失败,请重试',
-      //     icon: 'none',
-      //     duration: 2000
-      //   })
-      // })
+        this.setIndex(0)
+        // this.getdistance(list)
+  },
+
+  lastStep(){
+    if(this.data.index===0){
+      app.show('已最早')
+      return 
+    }
+    this.setIndex(this.data.index-1)
+  },
+
+  nextStep(){
+    if (this.data.index === this.data.list.length-1) {
+      app.show('已最迟')
+      return 
+    }
+    this.setIndex(this.data.index + 1)
+  },
+
+  setIndex(index) {
+    let list=this.data.list.map(res=>{
+      return {
+        ...res
+      }
+    })
+    app.showLoading('请稍后')
+    list[index]={
+      ...list[index],
+      iconPath: '../../../image/message_index.png',
+      width: 22,
+      height: 30,
+      anchor: { x: 0.5, y: 1 }
+    }
+    app.mapApi([list[index]]).then(ress => {
+      app.hideLoading()
+      this.setData({
+        index,
+        time: list[index].eventTime,
+        longitude: list[index].longitude,
+        latitude: list[index].latitude,
+        isWifi: list[index].wifiGpsFlag,
+        polyline: [{
+            points: list.slice(0,index+1),
+            color: '#02ad00',
+            arrowLine: true,
+            width: 3,
+          },
+          {
+            points: list.slice(index),
+            color: '#25321C',
+            arrowLine: true,
+            width: 3,
+          }],
+        startName: ress[0],
+        markerDatas: list
+      })
+    })
+  },
+
+  markertap(e) {
+    this.setIndex(e.markerId)
   },
 
   getdistance(list){
