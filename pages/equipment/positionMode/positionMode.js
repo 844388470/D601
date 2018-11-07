bd91cc6175a471cf878029d5c7488e57699fbe70
lq <844388470@qq.com>
2018-11-07 17:22:18 +0800

    Modify:定位模式下发修改

--- pages/equipment/positionMode/positionMode.js
+++ pages/equipment/positionMode/positionMode.js
@@ -46,18 +46,26 @@ Page({
   editMode(value){
     app.showLoading('修改中')
     app.request({
-      url: `${app.api.setEqu}${app.nowCodeList[app.equIndex].id}`,
-      method: 'put',
+      url: `${app.api.setEqu}${app.nowCodeList[app.equIndex].id}/commands`,
+      method: 'POST',
       data: {
-        locate_mode: value
+        f:'5',
+        d: `${value}`
       }
     }).then(res => {
-      wx.hideLoading()
-      app.nowCodeList[app.equIndex].locate_mode = Number(value)
-      this.setActive()
+      app.show('指令已下发,等待设备响应')
+      // wx.hideLoading()
+      // if(){
+
+      // }
+      // app.nowCodeList[app.equIndex].locate_mode = Number(value)
+      // this.setActive()
     }).catch(err => {
-      wx.hideLoading()
-      app.show('修改失败')
+      if (err && (err =='Can not send same command twice')){
+        app.show('等待设备响应中')
+      }else{
+        app.show('修改失败')
+      }
     })
   },
 
@@ -70,53 +78,6 @@ Page({
       return
     }
     this.editMode(1)
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
+  
 })
